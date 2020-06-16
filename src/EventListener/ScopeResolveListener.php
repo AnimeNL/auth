f807232e14bc7761b769a058a3de852bb6144861
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-06-16 21:21:53 +0200

    Fix scopes and refresh, update grumphp

--- src/EventListener/ScopeResolveListener.php
+++ src/EventListener/ScopeResolveListener.php
@@ -2,19 +2,22 @@
 
 namespace App\EventListener;
 
+use App\Entity\Anplan\Scope;
 use App\Normalizer\ScopeNormalizer;
 use App\Provider\UserProvider;
+use App\Repository\Anplan\ScopeRepository;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Trikoder\Bundle\OAuth2Bundle\Event\ScopeResolveEvent;
-use Trikoder\Bundle\OAuth2Bundle\Model\Scope;
 
 final class ScopeResolveListener
 {
     private UserProvider $userProvider;
+    private ScopeRepository $scopeRepository;
 
-    public function __construct(UserProvider $userProvider)
+    public function __construct(UserProvider $userProvider, ScopeRepository $scopeRepository)
     {
         $this->userProvider = $userProvider;
+        $this->scopeRepository = $scopeRepository;
     }
 
     public function onScopeResolve(ScopeResolveEvent $event): void
@@ -23,7 +26,10 @@ final class ScopeResolveListener
         if (!$user instanceof UserInterface) {
             return;
         }
-        $scopes = array_map(fn (Scope $scope) => ScopeNormalizer::normalize($scope), $user->getRoles());
+        $scopes = array_map(
+            fn (Scope $scope) => ScopeNormalizer::normalize($scope),
+            $this->scopeRepository->getUserScopes($user->getId(), $event->getScopes())
+        );
         $event->setScopes(...$scopes);
     }
 }
--- src/Normalizer/ScopeNormalizer.php
+++ src/Normalizer/ScopeNormalizer.php
@@ -2,11 +2,12 @@
 
 namespace App\Normalizer;
 
+use App\Entity\Anplan\Scope as AnplanScope;
 use Trikoder\Bundle\OAuth2Bundle\Model\Scope;
 
 class ScopeNormalizer
 {
-    public static function normalize(Scope $scope): Scope
+    public static function normalize(AnplanScope $scope): Scope
     {
         return new Scope(str_replace(' ', '_', $scope));
     }
