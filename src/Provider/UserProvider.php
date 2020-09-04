a9182421348354fa353748937268c189f931c32f
Jan Vernieuwe (PHPro) <jan.vernieuwe@phpro.be>
2020-09-04 11:00:09 +0200

    Update packages

--- src/Provider/UserProvider.php
+++ src/Provider/UserProvider.php
@@ -4,7 +4,7 @@ namespace App\Provider;
 
 use App\Entity\Anplan\User;
 use App\Repository\Anplan\UserRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
